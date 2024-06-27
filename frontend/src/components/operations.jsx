c4fe6fabe53d9c3acf0f43a6b0085652396650be
Victor Porres Bertomeu <victorpb@me.com>
2024-06-27 05:15:59 +0200

    chain data in operations module

--- frontend/src/components/operations.jsx
+++ frontend/src/components/operations.jsx
@@ -1,21 +1,59 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Header } from './header';
 import { Link, useParams } from 'react-router-dom'
+import axios from 'axios';
 
 
 export function Operations() {
     const params = useParams()
     const id = params.id
+    let initialData = {
+        id: '',
+        chainId: '',
+        subnet: '',
+        ipBootnode: '',
+        alloc: [''],
+        nodos: [{ id: '', type: '', name: '', ip: '', port: '' }]
+      };
+    const [formData, setFormData] = useState(initialData);
+
+    useEffect(() => {
+        const fetchData = async () => {
+          if (id) {
+            try {
+              const headers = {
+                'Content-Type': 'application/json',
+              };
+              const response = await axios.get(`http://localhost:3000/api/v1/networks/${id}`, { headers });
+              const data = response.data;
+              setFormData({
+                id: data.id,
+                chainId: data.chainId,
+                subnet: data.subnet,
+                ipBootnode: data.ipBootnode,
+                alloc: data.alloc,
+                nodos: data.nodos
+              });
+            } catch (error) {
+              console.error('Error al obtener los datos:', error);
+            }
+          }
+        };
+    
+        fetchData();
+      }, [id]);
+
+
     //console.log(id)
     //Aqui debemos hacer consulta a server para recuperar datos de la red
     //ahora los simulo con un objeto llamado test
-    const test = {
-        "status":"UP",
-        "id":"1234",
-        "chain":"chain_XXX",
-        "subnet":"192.168.0.1",
-        "bootnode":1234
-      }
+    // const test = {
+    //     "status":"UP",
+    //     "id":"1234",
+    //     "chain":"chain_XXX",
+    //     "subnet":"192.168.0.1",
+    //     "bootnode":1234
+    //   }
 
   return (
     <div>
@@ -33,8 +71,8 @@ export function Operations() {
         </div>
 
         <div>
-        <h3 className={`mx-3 mt-4 ${test.status === "DOWN" ? "text-danger" : test.status === "UP" ? "text-success" : ""}`}>
-            Datos de la red {test.id} -- {test.status}
+        <h3 className={`mx-3 mt-4 ${formData.status === "DOWN" ? "text-danger" : formData.status === "UP" ? "text-success" : ""}`}>
+            Datos de la red {formData.id} -- {formData.status}
         </h3>
 
             <table className="mx-3 table">
@@ -48,12 +86,12 @@ export function Operations() {
                 </tr>
             </thead>
             <tbody className="table-group-divider" >
-                    <tr key={test.id}>
-                    <td>{test.id}</td>
-                    <td>{test.status}</td>
-                    <td>{test.chain}</td>
-                    <td>{test.subnet}</td>
-                    <td>{test.bootnode}</td>
+                    <tr key={formData.id}>
+                    <td>{formData.id}</td>
+                    <td>{formData.status}</td>
+                    <td>{formData.chainId}</td>
+                    <td>{formData.subnet}</td>
+                    <td>{formData.ipBootnode}</td>
                 </tr>
             </tbody>
             </table>
